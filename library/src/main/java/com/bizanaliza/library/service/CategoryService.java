20a7f42ef60cfda062496d4cfc5a2dbdf93d9d38
Borivoj Grujicic <94956337+DataBora@users.noreply.github.com>
2023-08-23 21:19:10 +0200

    Update CategoryService.java

--- library/src/main/java/com/bizanaliza/library/service/CategoryService.java
+++ library/src/main/java/com/bizanaliza/library/service/CategoryService.java
@@ -4,10 +4,31 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import com.bizanaliza.library.domain.Category;
+import com.bizanaliza.library.domain.CategoryQueries;
+import com.bizanaliza.library.rest.ErrorMessages;
+
 @Stateless
 public class CategoryService {
 
 	@PersistenceContext
 	private EntityManager em;
 	
+	public Category createCategory(Category category) {
+		
+		if(category.getName().length() > 50) {
+			throw new AppException(ErrorMessages.category_name_tooLong);
+		}
+		
+		Category c = CategoryQueries.findByName(em, category.getName());
+		
+		if(c != null) {
+			throw new AppException(ErrorMessages.category_name_exist);
+		}
+		
+		em.persist(category);
+		return category;
+	}
+	
 }
+ 
