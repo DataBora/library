0131f191d0747752e42e7e5cb2d9aeb1ec9fb372
Borivoj Grujicic <94956337+DataBora@users.noreply.github.com>
2023-08-23 21:17:27 +0200

    Update CategoryResource.java

--- library/src/main/java/com/bizanaliza/library/rest/CategoryResource.java
+++ library/src/main/java/com/bizanaliza/library/rest/CategoryResource.java
@@ -1,8 +1,14 @@
 package com.bizanaliza.library.rest;
 
 import javax.ejb.EJB;
+import javax.persistence.PersistenceException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+import com.bizanaliza.library.domain.Category;
+import com.bizanaliza.library.service.AppException;
 import com.bizanaliza.library.service.CategoryService;
 
 @Path("category")
@@ -10,5 +16,32 @@ public class CategoryResource {
 	
 	@EJB
 	private CategoryService categoryService;
+	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	public CreateCategoryResponse createCategory(Category category) {
+		
+		CreateCategoryResponse response = new CreateCategoryResponse();
+		
+		try {
+			Category c = categoryService.createCategory(category);
+			
+			response.setCategory(c);
+			//kad je sve ok
+			response.setErrorCode(ErrorMessages.ok);
+			
+			return response;
+			
+		} catch (PersistenceException pe) {
+			//kad uhvati error
+			response.setErrorCode(ErrorMessages.db_problem);
+			return response;
+		} catch (AppException appEx) {
+			response.setErrorCode(appEx.getError());
+			return response;
+		}
+		
+	}
 
 }
